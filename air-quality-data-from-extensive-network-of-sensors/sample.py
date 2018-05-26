459a506aaaf3add4e2be939ae719b9c39b36661c
michal <michallis95@vp.pl>
2018-05-26 16:35:19 +0200

    parameters per day

--- air-quality-data-from-extensive-network-of-sensors/sample.py
+++ air-quality-data-from-extensive-network-of-sensors/sample.py
@@ -1,8 +1,13 @@
 import pandas as pd
-
+import seaborn as sns
+from matplotlib import pyplot as plt
 sensors_df = pd.read_csv("sensor_locations.csv")
-print(sensors_df.head())
-print(sensors_df.shape)
-print(sensors_df.tail())
+
 
 december_df = pd.read_csv("december-2017.csv")
+print(december_df.columns.values)
+
+df_140 = december_df[['UTC time', '140_temperature', '140_humidity', '140_pressure', '140_pm1', '140_pm10', '140_pm25']]
+print(df_140.head())
+
+# sns.lmplot(x='UTC time', y='140_humidity', data=df_140)
